4cb6bd89d580bfd286406690e554d1b37f3a0e2a
Sanjar17 <sanjarsaidov173@gmail.com>
2023-05-26 15:36:54 +0500

    readme file

---
+++ lib/writereadme.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  File f2 = File('lib/homework.txt');
+  String text = f2.readAsStringSync();
+  List<String> lines = text.split('\n');
+  for (int i = 0; i < lines.length; i++) {
+    print(i);
+    if (lines[i].contains(RegExp('^Task [0-9]+:'))) {
+      print(lines[i]);
+      File f = File('README.md');
+      f.writeAsStringSync("# ${lines[i]}\n## ${lines[i + 1]}\n", mode: FileMode.append);
+    }
+  }
+}
